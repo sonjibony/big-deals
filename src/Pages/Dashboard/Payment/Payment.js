2a0a82d8a1e7a163fc560f23f93bb76da8c88d61
sonjibony <sonjibony@gmail.com>
2022-11-28 17:39:54 +0600

    checkout page edited

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -15,10 +15,11 @@ const Payment = () => {
   }
 
   return (
-    <div>
-      <h3 className="text-4xl">Payment for {product}</h3>
+    <div className="w-11/12 mx-auto">
+      <h1 className="text-5xl text-center my-5 font-bold text-primary">CHECKOUT</h1>
+      <h3 className="text-2xl mb-4">Payment for {product}</h3>
       <p className="text-xl">
-        Please pay <strong>${price}</strong>{" "}
+        Please pay <strong>${price}</strong>{" "} and confirm your purchase. Thank you.
       </p>
       <div className="w-96 my-12">
         <Elements stripe={stripePromise}>
